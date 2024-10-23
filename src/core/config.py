79c1fca2d19922265fc5c0386b40ed8e77e8a953
VitaliiMarlian <vmarlyan@mail.ru>
2024-10-23 23:07:26 +0300

    Add function to get env file paths

--- src/core/config.py
+++ src/core/config.py
@@ -1,6 +1,7 @@
 import os
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Tuple
 
 
 class RunConfig(BaseModel):
@@ -24,12 +25,18 @@ class DBConfig(BaseModel):
     DB_PORT: int
 
 
+def get_env_file_paths(*file_names: str) -> Tuple[str, ...]:
+    directory = os.path.dirname(os.path.abspath(__file__))
+    env_file_paths = tuple(os.path.join(directory, "../../", file_name) for file_name in file_names)
+    return env_file_paths
+
+
 class Settings(BaseSettings):
     db_config: DBConfig
     engine_config: EngineConfig = EngineConfig()
     run_config: RunConfig = RunConfig()
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../", ".env"),
+        env_file=get_env_file_paths(".env.template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__"
     )
